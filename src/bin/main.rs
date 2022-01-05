5d4555cc098d7c49221034757b577c25bb6bea6f
alexeykarnachev <alekseykarnachev@gmail.com>
2022-01-05 05:11:20 +0300

    add random scene generator

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,55 +1,21 @@
-use rust_raytracer::{
-    camera::Camera,
-    image::Image,
-    surfaces::{Material, Sphere, Surfaces},
-    vec3::Vec3,
-};
+use rust_raytracer::{camera::Camera, image::Image, surfaces::Surfaces, vec3::Vec3};
 
 fn main() {
-    let nx = 800;
-    let ny = 400;
-    let lookfrom = Vec3::new(-2.0, 2.0, 1.0);
+    let nx = 1400;
+    let ny = 700;
+    let lookfrom = Vec3::new(15.0, 5.0, 5.0);
     let lookat = Vec3::new(0.0, 0.0, -1.0);
     let camera = Camera::new(
         lookfrom,
         lookat,
         Vec3::new(0.0, 1.0, 0.0),
-        45.0,
+        30.0,
         nx as f32 / ny as f32,
-        0.0,
+        0.2,
         (lookfrom - lookat).length(),
     );
 
-    let surfaces = Surfaces::new(vec![
-        Box::new(Sphere::new(
-            Vec3::new(0.0, 0.0, -1.0),
-            0.5,
-            Material::Lambertian {
-                attenuation: Vec3::new(0.1, 0.2, 0.5),
-            },
-        )),
-        Box::new(Sphere::new(
-            Vec3::new(0.0, -100.5, -1.0),
-            100.0,
-            Material::Lambertian {
-                attenuation: Vec3::new(0.8, 0.8, 0.0),
-            },
-        )),
-        Box::new(Sphere::new(
-            Vec3::new(1.0, 0.0, -1.0),
-            0.5,
-            Material::Metal {
-                attenuation: Vec3::new(0.8, 0.6, 0.2),
-                fuzz: 0.2,
-            },
-        )),
-        Box::new(Sphere::new(
-            Vec3::new(-1.0, 0.0, -1.0),
-            0.5,
-            Material::Dielectric { ref_k: 2.0 },
-        )),
-    ]);
-
+    let surfaces = Surfaces::generate();
     let mut image = Image::new(nx, ny);
     image.render(&camera, &surfaces, 12);
     image.to_ppm("./render.ppm".to_string());
--- src/surfaces.rs
+++ src/surfaces.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 use crate::{ray::Ray, vec3::Vec3};
 
 pub trait Hit {
@@ -6,16 +8,9 @@ pub trait Hit {
 
 #[derive(Clone, Copy)]
 pub enum Material {
-    Lambertian {
-        attenuation: Vec3,
-    },
-    Metal {
-        attenuation: Vec3,
-        fuzz: f32,
-    },
-    Dielectric {
-        ref_k: f32,
-    },
+    Lambertian { attenuation: Vec3 },
+    Metal { attenuation: Vec3, fuzz: f32 },
+    Dielectric { ref_k: f32 },
 }
 
 pub struct HitRes {
@@ -83,6 +78,83 @@ pub struct Surfaces {
 
 impl Surfaces {
     pub fn new(surfaces: Vec<Box<dyn Hit + Sync>>) -> Self {
+        Self { surfaces }
+    }
+
+    pub fn generate() -> Self {
+        let mut rng = rand::thread_rng();
+        let mut rnd = || rng.gen::<f32>();
+
+        let mut surfaces: Vec<Box<dyn Hit + Sync>> = vec![];
+
+        for a in -11..11 {
+            for b in -11..11 {
+                let center = Vec3::new(a as f32 + 0.9 * rnd(), 0.2, b as f32 + 0.9 * rnd());
+                if (center - Vec3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                    let choose_mat = rnd();
+                    if choose_mat < 0.8 {
+                        surfaces.push(Box::new(Sphere::new(
+                            center,
+                            0.2,
+                            Material::Lambertian {
+                                attenuation: Vec3::new(rnd() * rnd(), rnd() * rnd(), rnd() * rnd()),
+                            },
+                        )));
+                    } else if choose_mat < 0.95 {
+                        surfaces.push(Box::new(Sphere::new(
+                            center,
+                            0.2,
+                            Material::Metal {
+                                attenuation: Vec3::new(
+                                    0.5 * (1.0 + rnd()),
+                                    0.5 * (1.0 + rnd()),
+                                    0.5 * (1.0 + rnd()),
+                                ),
+                                fuzz: 0.5 * rnd(),
+                            },
+                        )));
+                    } else {
+                        surfaces.push(Box::new(Sphere::new(
+                            center,
+                            0.2,
+                            Material::Dielectric { ref_k: 1.5 },
+                        )))
+                    }
+                }
+            }
+        }
+
+        surfaces.push(Box::new(Sphere::new(
+            Vec3::new(0.0, -1000.0, 0.0),
+            1000.0,
+            Material::Lambertian {
+                attenuation: Vec3::new(0.1, 0.5, 0.1),
+            },
+        )));
+
+        surfaces.push(Box::new(Sphere::new(
+            Vec3::new(0.0, 1.0, 0.0),
+            1.0,
+            Material::Dielectric { ref_k: 1.5 },
+        )));
+
+        surfaces.push(Box::new(Sphere::new(
+            Vec3::new(-4.0, 1.0, 0.0),
+            1.0,
+            Material::Lambertian {
+                attenuation: Vec3::new(0.4, 0.2, 0.1),
+            },
+        )));
+
+        surfaces.push(Box::new(Sphere::new(
+            Vec3::new(4.0, 1.0, 0.0),
+            1.0,
+            Material::Metal {
+                attenuation: Vec3::new(0.7, 0.6, 0.5),
+                fuzz: 0.5 * rnd(),
+            },
+        )));
+
         Self { surfaces }
     }
 
